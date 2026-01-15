2b2a9dce2c6951df6168e65a5233cd3d3953eeda
cm-collins <dev-collins@outlook.com>
2026-01-15 18:08:35 +0300

    work with the decimal  (Floats and Double )

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,51 @@
 fun main () {
 //comparison operators and logical operators
-    val x = 10
-    val y = 11
+//    val x = 10
+//    val y = 11
+//
+//    fun AretheResultsTrue ()
+//    {
+//         if (x % 2 == 0)
+//         {
+//             print("x is even")
+//         }
+//        else if (x % 2 == 1)
+//         {
+//            print("x is odd")
+//         }
+//        else if (x % 2 == 2)
+//         {
+//            print("x is even")
+//         }
+//        else if (x % 2 == 3)
+//         {
+//            print("x is odd")
+//         }
+//
+//    }
+//
+//    return AretheResultsTrue()
 
-    fun AretheResultsTrue ()
-    {
-         if (x % 2 == 0)
-         {
-             print("x is even")
-         }
-        else if (x % 2 == 1)
-         {
-            print("x is odd")
-         }
-        else if (x % 2 == 2)
-         {
-            print("x is even")
-         }
-        else if (x % 2 == 3)
-         {
-            print("x is odd")
-         }
 
-    }
+    //Decimal numbers
 
-    return AretheResultsTrue()
+    //float
+//    var x  = 20.06874763469043869089067439067943068F
+//    var y = 8.068347634653864634F
+
+    //Double
+//        var x  = 20.06874763469043869089067439067943068
+//    var y = 8.068347634653864634
+
+
+//    println("The result of x / y is ${x / y}")
+    //Exercise , calculating the volume of a sphere (0.75 * pi * radius ^3)
+
+    val pi = 3.142857
+    val radius = 5.5
+
+    val  volume =  (0.75 * pi  * (radius * radius * radius))
+
+    println("The voulume of the sphere with the radius ${radius} is  ${volume}" + "cm3:")
 
 }
