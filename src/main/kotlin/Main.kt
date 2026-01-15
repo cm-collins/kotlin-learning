c8acf3fedd09db96b742b28fba443388df00fd4a
cm-collins <dev-collins@outlook.com>
2026-01-15 08:49:25 +0300

    learning kotlin operation

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main () {
+//comparison operators and logical operators
+    val x = 10
+    val y = 11
+
+    fun AretheResultsTrue ()
+    {
+         if (x % 2 == 0)
+         {
+             print("x is even")
+         }
+        else if (x % 2 == 1)
+         {
+            print("x is odd")
+         }
+        else if (x % 2 == 2)
+         {
+            print("x is even")
+         }
+        else if (x % 2 == 3)
+         {
+            print("x is odd")
+         }
+
+    }
+
+    return AretheResultsTrue()
+
+}
